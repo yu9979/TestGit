1979b172d7cadcc1d53c4ba01f0255a6d07f1209
wangjinyu <763975228@qq.com>
2018-11-15 11:29:13 +0800

    测试git

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by wangjinyu on 2018/11/15 11:28.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("*-****");
+    }
+}
